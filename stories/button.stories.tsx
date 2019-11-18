bfcc1c7eff158165fc17f7cd22df1fa06daef9ba
StoneT2000 <stoneztao@gmail.com>
2019-11-17 17:59:10 -0800

    Initial

---
+++ stories/button.stories.tsx
@@ -0,0 +1,15 @@
+import { storiesOf } from "@storybook/react";
+import * as React from "react";
+import 'antd/dist/antd.css';
+import { Button } from "../";
+
+storiesOf("Button", module).add("All", () => (
+  <div className="storycontainer">
+    <div className="story-module spaced">
+      <h3>Small Buttons (fixed)</h3>
+      <Button type="primary" size="small">
+        CLICK ME
+      </Button>
+    </div>
+  </div>
+));
