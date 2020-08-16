940f8f9888a7d3b44e26eb78b344b84290858192
Stone Tao <stonezt2019@gmail.com>
2020-08-16 11:00:56 -0700

    buttons

--- components/Button/index.tsx
+++ components/Button/index.tsx
@@ -7,15 +7,23 @@ type ButtonProps = {
   className?: string,
   type?: string,
   size?: string,
+  children: React.ReactNode
 }
 
 /* Default Button is medium sized */
-export const Button: React.FunctionComponent<ButtonProps> = ({ className, type, children }) => {
-  let classes = classnames("acm-btn", {"acm-btn-primary": type === "primary"});
+export const Button = ({ className, type, size, children }: ButtonProps) => {
+  let classes = classnames("acm-btn", 
+    {
+      "acm-btn-primary": type === "primary",
+      "acm-btn-outline": type === "outline",
+      "acm-btn-small": size === "small",
+      "acm-btn-medium": size === "medium",
+      "acm-btn-large": size === "large"
+    }
+  );
   if (className) {
     classes = classnames(classes, ...className.split(" "));
   }
-  console.log(classes);
   return (
     <button className={classes}>
       <span>{children}</span>
--- stories/button.stories.tsx
+++ stories/button.stories.tsx
@@ -1,13 +1,30 @@
 import { storiesOf } from "@storybook/react";
 import * as React from "react";
 import 'antd/dist/antd.css';
-import { Button } from "../";
+import { Button } from "../components/Button";
 
 storiesOf("Button", module).add("All", () => (
   <div className="storycontainer">
-    <div className="story-module spaced">
+    <div className="story-module spaced" style={{backgroundColor:'rgba(10,190,240, 0.25)'}}>
       <h3>Small Buttons</h3>
       <Button type="primary" size="small">
+        CLICK ME
+      </Button>
+      <Button type="outline" size="small">
+        CLICK ME
+      </Button>
+      <h3>Medium Buttons</h3>
+      <Button type="primary" size="medium">
+        CLICK ME
+      </Button>
+      <Button type="outline" size="small">
+        CLICK ME
+      </Button>
+      <h3>Large Buttons</h3>
+      <Button type="primary" size="large">
+        CLICK ME
+      </Button>
+      <Button type="outline" size="small">
         CLICK ME
       </Button>
     </div>
